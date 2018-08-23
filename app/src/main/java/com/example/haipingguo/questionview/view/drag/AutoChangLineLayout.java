f4b82cc2165a2e2bd38094e7cb320311d561d44f
guohaiping <guohaiping@zuoyebang.com>
2018-08-23 19:20:18 +0800

    Modify:换行代码init

--- app/src/main/java/com/example/haipingguo/questionview/view/drag/AutoChangLineLayout.java
+++ app/src/main/java/com/example/haipingguo/questionview/view/drag/AutoChangLineLayout.java
@@ -36,27 +36,30 @@ public class AutoChangLineLayout extends ViewGroup {
         return new MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
     }
 
+    int line;
+    int sizeWidth;
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int totalWidth = 0;
         int totalHeight = 0;
-        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
         int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
         int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
         int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
-        measureChildren(widthMeasureSpec, heightMeasureSpec);
-        //measureChildren(widthMeasureSpec,heightMeasureSpec);
+        measureChildren(widthMeasureSpec,heightMeasureSpec);
         for (int i = 0; i < getChildCount(); ++i) {
             View child = getChildAt(i);
-            if(child.getVisibility()!=View.GONE){
-                measureChild(child,widthMeasureSpec,heightMeasureSpec);
-            }
             MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
             totalWidth += child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;
             if(totalHeight<child.getMeasuredHeight()+lp.topMargin+lp.bottomMargin){
                 totalHeight = child.getMeasuredHeight()+lp.topMargin+lp.bottomMargin;
             }
-            Log.i("ghppp","onMeasure child width=="+child.getMeasuredWidth());
+            if(totalWidth>sizeWidth){
+                line=totalWidth/sizeWidth+1;
+                totalHeight=totalHeight+(totalWidth/sizeWidth)*totalHeight;
+                Log.i("ghpppp","(totalWidth/sizeWidth)=="+(totalWidth/sizeWidth));
+            }
         }
         int width = modeWidth==MeasureSpec.AT_MOST?totalWidth:sizeWidth;
         int height = modeHeight==MeasureSpec.AT_MOST?totalHeight:sizeHeight;
@@ -75,44 +78,40 @@ public class AutoChangLineLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int totalWidth = 0;
+        int totalHeight = 0;
         int left = 0, top = 0,right=0,rightMargin=0;
+
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
+
+
             MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
-            top=lp.topMargin;
-            left =i==0?lp.leftMargin:left+child.getMeasuredWidth()+lp.leftMargin+rightMargin;
-            right=left + child.getMeasuredWidth();
-            child.layout(left, top, right,
-                    top + child.getMeasuredHeight());
-            rightMargin=lp.rightMargin;
-        }
-    }
-
-    private int getMeasureHeightSize(boolean isChangeLine, int aSize) {
-        if (isChangeLine) {
-
-        }
-        return 0;
-    }
-
-    private int getMeasureWidthSize(int aSize) {
-        int size = 0;
-        for (int i = 0; i < getChildCount(); i++) {
-            View child = getChildAt(i);
-            MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
-            if (i == 0) {
-                size = child.getMeasuredWidth();
-            } else {
-                size = child.getMeasuredWidth() + layoutParams.leftMargin;
+            totalWidth += child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;
+            if(totalHeight<child.getMeasuredHeight()+lp.topMargin+lp.bottomMargin){
+                totalHeight = child.getMeasuredHeight()+lp.topMargin+lp.bottomMargin;
             }
+            if(totalWidth>sizeWidth){
+                line=totalWidth/sizeWidth+1;
+                totalHeight=totalHeight+(totalWidth/sizeWidth)*totalHeight;
+            }
+            if(line==1){
+                top=lp.topMargin;
+                left =i==0?lp.leftMargin:left+child.getMeasuredWidth()+lp.leftMargin+rightMargin;
+                right=left + child.getMeasuredWidth();
+                child.layout(left, top, right,
+                        top + child.getMeasuredHeight());
+                rightMargin=lp.rightMargin;
+            }else if(line==2){
+                top=lp.topMargin;
+                left =i==0?lp.leftMargin:left+child.getMeasuredWidth()+lp.leftMargin+rightMargin;
+                right=left + child.getMeasuredWidth();
+                child.layout(left, top, right,
+                        top + child.getMeasuredHeight());
+                rightMargin=lp.rightMargin;
+            }
+
         }
-        return size;
     }
-
-    /*Left position, relative to parent
-     * @param t Top position, relative to parent
-     * @param r Right position, relative to parent
-     * @param b Bottom position, relative to parent*/
-
 
 }
